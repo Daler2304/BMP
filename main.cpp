35a19edba6fa78b4d88175cd41ea4920cc209532
Daler2304 <110163963+Daler2304@users.noreply.github.com>
2023-12-29 10:23:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ int main() {
     
     BMP bmp1("im.bmp");
     
-    // Поворот на 90 градусов по часовой стрелке
+    //Поворот на 90 градусов по часовой стрелке
     bmp1.rotate_clockwise_90();
     bmp1.write("output_clockwise.bmp");
 
-    
+    //Против часовой
     BMP bmp2("im.bmp");
     bmp2.rotate_counterclockwise_90();
     bmp2.write("output_counterclockwise.bmp");
